76c8080e8a0c74fda83b056f69990a3021d6f87f
Chirag <chiragsagar1506@gmail.com>
2024-11-19 01:35:51 -0500

    Update test files

--- flask_app/tests/test_db_read.py
+++ flask_app/tests/test_db_read.py
@@ -1,16 +1,8 @@
-# Chirag Tarsemlal Sagar.
-
-from pymongo import MongoClient
+import sys
 import os
-from dotenv import load_dotenv
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pymongo import MongoClient
-from urllib.parse import quote_plus
-
-load_dotenv()
-username = quote_plus(os.getenv('MONGODB_USERNAME')) 
-password = quote_plus(os.getenv('MONGODB_PASSWORD'))
-
-connection_string = f"mongodb+srv://{username}:{password}@mystoredb.qodsv.mongodb.net/shop_db?retryWrites=true&w=majority&appName=mystoredb"
+from app import connection_string
 
 def test_mongodb_connection():
     client = MongoClient(connection_string)
--- flask_app/tests/test_db_write.py
+++ flask_app/tests/test_db_write.py
@@ -1,37 +1,18 @@
-# Chirag Tarsemlal Sagar.
-
 from pymongo import MongoClient
+import sys
 import os
-from dotenv import load_dotenv
-from pymongo import MongoClient
-from urllib.parse import quote_plus
-load_dotenv()
-username = quote_plus(os.getenv('MONGODB_USERNAME')) 
-password = quote_plus(os.getenv('MONGODB_PASSWORD'))
-
-connection_string = f"mongodb+srv://{username}:{password}@mystoredb.qodsv.mongodb.net/shop_db?retryWrites=true&w=majority&appName=mystoredb"
-
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from app import connection_string
 
 def test_mongodb_write_operation():
     client = MongoClient(connection_string)
     db = client['shop_db']
     products_collection = db['products']
 
-    test_product = {
-        "name": "Test Watch",
-        "price": 99.99,
-        "tag": "Test Watch",
-        "image_path": "images/smartwatch.jpg" 
-    }
+    test_product = {"name": "Test Watch", "price": 99.99, "tag": "Test Watch"}
     result = products_collection.insert_one(test_product)
 
     inserted_product = products_collection.find_one({"_id": result.inserted_id})
     assert inserted_product['name'] == "Test Watch"
-    assert inserted_product['price'] == 99.99
-    assert inserted_product['tag'] == "Test Watch"
-    assert inserted_product['image_path'] == "images/smartwatch.jpg"
 
     products_collection.delete_one({"_id": result.inserted_id})
-
-    deleted_product = products_collection.find_one({"_id": result.inserted_id})
-    assert deleted_product is None
