17503a811d1ea7d43b3eb606e5dd2b9f8d490117
Chirag <chiragsagar1506@gmail.com>
2024-11-19 02:01:33 -0500

    Update test file

--- flask_app/tests/test_db_read.py
+++ flask_app/tests/test_db_read.py
@@ -5,7 +5,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from app import connection_string
 
 def test_mongodb_connection():
-    """Test MongoDB connection using ping."""
     client = MongoClient(connection_string)
     response = client.admin.command('ping')
     assert response['ok'] == 1.0
